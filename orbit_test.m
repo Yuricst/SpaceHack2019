a3ca76b0e4a3cf6176d83f63c08cb6b7095a8359
Yuri@Surface <yuri.shimane@gmail.com>
2019-11-09 11:17:15 +0000

    Create orbit_test.m

---
+++ orbit_test.m
@@ -0,0 +1,15 @@
+%% Molniya
+
+% house keeping
+clear; close all; clc;
+
+%% Add path to spice
+addpath('C:\Users\yurio\Documents\MATLAB\mice\src\mice\')
+addpath('C:\Users\yurio\Documents\MATLAB\mice\lib\' )
+
+% test run
+which mice
+
+%% Define orbit
+
+
